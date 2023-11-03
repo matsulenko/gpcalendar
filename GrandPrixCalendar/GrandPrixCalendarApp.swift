d8b2e9dddb59b3ba676b0bb293cdfbe9048c5ee0
matsulenko <matsulenko@mail.ru>
2023-11-03 20:26:20 +0300

    Initial Commit

---
+++ GrandPrixCalendar/GrandPrixCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrandPrixCalendarApp.swift
+//  GrandPrixCalendar
+//
+//  Created by Matsulenko on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GrandPrixCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
